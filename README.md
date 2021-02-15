afea2a155de92bb30bc445b6aad130697259d71f
janarth10 <janarthp10@gmail.com>
2021-02-14 19:15:47 -0500

    wip

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 ## goal
 - every week automatically download the latest chapter of one piece then upload it to my kobo
 
+## currently working
+1. launchctl (macOs version of cron jobs) to run the manga-downloader-v2.py script on the 5th minute of
+    every hour
+2. PIL to combine images into a PDF
+3. beautifulsoup to parse through HTML of webpage to find all images with a given class attr (have
+to find this for every manga I'm trying to download)
+
 ## steps
 1. given a url for first image, download all images for chapter
 2. combine images into a pdf named onepiece-chapter-{}.pdf
--- manga-downloader-v2.py
+++ manga-downloader-v2.py
@@ -7,10 +7,41 @@ import bs4
 import requests  # to get image from the web
 from PIL import Image
 
+from twilio.rest import Client
+
+# # defaulting to 15 second timeout on requests to Twilio API. Our default timeout for all requests is
+# # either 15 or 20 seconds (based on tickets/server_configs/bigdata/app/nginx.conf)
+# def get_twilio_client():
+#     return Client(
+#         username='AC18bb3e5ff335f016fd116dbb8488caee',
+#         password='5059b3ac1419c554ad6590775bcf9bd5',
+#         # account_sid=subaccount_id,
+#         # http_client=TwilioHttpClient(timeout=timeout),
+#     )
+
+# def send_sms(
+#     message_body='',
+#     recipient_phone_num=''
+# ):
+#     twilio_client = get_twilio_client()
+#     kwargs = {
+#         'body': 'test_acc', # pdf attachment?
+#         'messaging_service_sid': 'MGbdf99407ee26f0bfdc6b62d04faec7b9',
+#         'to': '6475233013',
+#         'media_url': ['/Users/newdev/Hive/Development/personal_projects/manga-downloader/onepiece_chapter_1002.pdf']
+#     }
+
+#     response = twilio_client.messages.create(**kwargs)
+
+# send_sms()
+
+
+ABS_REPO_PATH = '/Users/newdev/Hive/Development/personal_projects/manga-downloader'
+LOG_FILE = f'{ABS_REPO_PATH}/logs.json'
 
 # Get chapter number from logs
 chapter_num = None
-with open("logs.json") as f:
+with open(LOG_FILE) as f:
     chapter_num = json.load(f).get("chapter_num")
 
 # Download webpage HTML
@@ -41,19 +72,19 @@ for i, img_url in enumerate(img_urls):
         r.raw.decode_content = True
 
         # Open a local file with wb ( write binary ) permission.
-        filename = img_url.split("/")[-1]
+        filename = f'{ABS_REPO_PATH}/{img_url.split("/")[-1]}'
         img_filename_list.append(filename)
         with open(filename, "wb") as f:
             shutil.copyfileobj(r.raw, f)
 
-        img_list.append(Image.open(filename))
+        img_list.append(Image.open(filename).convert('RGB'))
 
         print("Image sucessfully Downloaded: ", filename)
     else:
         error_msg = f"Image {i} Couldn't be retreived\nfailed for unexpected status code = {r.status_code}"
         print(error_msg)
-        with open("logs.json", "w") as f:
-            json.dump({"chapter_num": chapter_num, "error_msg": error_msg}, f)
+        with open(LOG_FILE, 'w') as f:
+            json.dump({"error_msg": error_msg, "chapter_num": chapter_num}, f)
         if i == 0:
             print(
                 "Failed on first image. Assuming manga didn't come out, not incrementing manga number"
@@ -64,7 +95,7 @@ for i, img_url in enumerate(img_urls):
 if len(img_urls) == len(
     img_list
 ):  # create pdf if we downloaded as many images as we expected
-    final_pdf_name = f"onepiece_chapter_{chapter_num}.pdf"
+    final_pdf_name = f"{ABS_REPO_PATH}/onepiece_chapter_{chapter_num}.pdf"
     im1 = img_list[0]
     im1.save(
         final_pdf_name,
@@ -74,7 +105,7 @@ if len(img_urls) == len(
         append_images=img_list[1:],
     )
 
-with open("logs.json", "w") as f:
+with open(LOG_FILE, 'w') as f:
     json.dump({"chapter_num": int(chapter_num) + 1}, f)
 
 # Clean up image downloads
