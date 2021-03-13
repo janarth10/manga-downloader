a799f153c522b902a91766bae7fe4a534c67c9a5
janarth10 <janarthp10@gmail.com>
2021-03-13 11:28:16 -0500

    black linting

--- manga-downloader-v2.py
+++ manga-downloader-v2.py
@@ -12,24 +12,24 @@ from sms import send_sms
 # %c format gives human readable string
 print(f"Last ran { datetime.datetime.now().strftime('%c') }")
 
-ABS_REPO_PATH = '/Users/newdev/Hive/Development/personal_projects/manga-downloader'
-MANGAS_CONFIG = f'{ABS_REPO_PATH}/configs/mangas.json'
-G_DRIVE_FOLDER_ID = '1jLJ69cn5FcEshFg-uk3kYdwq9-c_QAeZ' # ID to mangas folder
+ABS_REPO_PATH = "/Users/newdev/Hive/Development/personal_projects/manga-downloader"
+MANGAS_CONFIG = f"{ABS_REPO_PATH}/configs/mangas.json"
+G_DRIVE_FOLDER_ID = "1jLJ69cn5FcEshFg-uk3kYdwq9-c_QAeZ"  # ID to mangas folder
 
 # Get mangas to download from json
 mangas = None
 with open(MANGAS_CONFIG) as f:
-    mangas = json.load(f)['mangas']
+    mangas = json.load(f)["mangas"]
 
 
 # Downloading a specific manga through terminal
 if len(sys.argv) == 3:
-    print('arg list: ', str(sys.argv))
+    print("arg list: ", str(sys.argv))
     chapter_name = str(sys.argv[1])
     ch_num = int(sys.argv[2])
 
-    manga_data = [manga for manga in mangas if manga.get('name') == chapter_name][0]
-    manga_data['ch_num'] = ch_num
+    manga_data = [manga for manga in mangas if manga.get("name") == chapter_name][0]
+    manga_data["ch_num"] = ch_num
     manga_obj = Manga(**manga_data)
     pdf_path = manga_obj.download_chapter_into_pdf()
 
@@ -45,19 +45,19 @@ elif len(sys.argv) == 1:
 
             # upload to gdrive
             file_data = upload_file_to_drive(
-                drive_file_name=pdf_path.split('/')[-1],
+                drive_file_name=pdf_path.split("/")[-1],
                 file_path=pdf_path,
-                parents=[G_DRIVE_FOLDER_ID]
+                parents=[G_DRIVE_FOLDER_ID],
             )
 
             # send sms with gdrive links
             sms_body = f"New download: {file_data['name']}\nView Online: {file_data['webViewLink']}\n Download Manga: {file_data['webContentLink']}"
-            for phone_number in manga_data['phone_numbers']:
+            for phone_number in manga_data["phone_numbers"]:
                 send_sms(message_body=sms_body, recipient_phone_num=phone_number)
 
             # Incrementing this value essentially indicates the operations as a success.
             #   Because the script will only be trying to download the next manga in future executions.
-            mangas[i]['ch_num'] += 1
+            mangas[i]["ch_num"] += 1
 
         except:
             # blindly catching exceptions, don't wanna miss other mangas if one fails
@@ -66,8 +66,8 @@ elif len(sys.argv) == 1:
             pass
 
     # save config file with incremented chapter nums
-    with open(MANGAS_CONFIG, 'w') as f:
-        json.dump({'mangas': mangas}, f)
+    with open(MANGAS_CONFIG, "w") as f:
+        json.dump({"mangas": mangas}, f)
 
 else:
-    raise Exception('Unexpected amount of command line arguments!!')
+    raise Exception("Unexpected amount of command line arguments!!")
